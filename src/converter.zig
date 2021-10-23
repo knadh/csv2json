5d7ffd1d30905c2401217a03d9cafeedc097d968
Kailash Nadh <kailash@nadh.in>
2021-10-23 20:55:24 +0530

    Add project files.

---
+++ src/converter.zig
@@ -0,0 +1,176 @@
+const std = @import("std");
+const csv = @import("csv");
+
+pub const Converter = struct {
+    const BufferedWriter = std.io.BufferedWriter(4096, std.fs.File.Writer);
+
+    allocator: *std.mem.Allocator,
+    csvBuf: []u8,
+    hdrBuf: []u8,
+    rowBuf: []u8,
+    keys: [][]const u8,
+    out: BufferedWriter,
+
+    // Initialize the converter.
+    pub fn init(allocator: *std.mem.Allocator, writer: anytype, rowBufSize: u32) !Converter {
+        var s = Converter{
+            .allocator = allocator,
+            .out = std.io.bufferedWriter(writer),
+
+            // .csvBuf = try allocator.alloc(u8, rowBufSize),
+            .csvBuf = try allocator.alloc(u8, rowBufSize),
+            .hdrBuf = try allocator.alloc(u8, rowBufSize),
+            .rowBuf = try allocator.alloc(u8, rowBufSize),
+            .keys = undefined,
+        };
+
+        return s;
+    }
+
+    pub fn deinit(self: *Converter) void {
+        if (self.csvBuf) self.allocator.free(self.csvBuf);
+        if (self.hdrBuf) self.allocator.free(self.hdrBuf);
+        if (self.rowBuf) self.allocator.free(self.rowBuf);
+    }
+
+    // Convert a CSV file. rowBufSize should be greater than the length (bytes)
+    // of the biggest row in the CSV file.
+    pub fn convert(self: *Converter, filePath: []const u8) !u64 {
+        const file = try std.fs.cwd().openFile(filePath, .{});
+        defer file.close();
+        const tk = &try csv.CsvTokenizer(std.fs.File.Reader).init(file.reader(), self.csvBuf, .{});
+
+        var fields = std.ArrayList([]const u8).init(std.heap.page_allocator);
+        var isFirst: bool = true;
+        var line: u64 = 1;
+        var f: usize = 0;
+
+        while (try tk.next()) |token| {
+            switch (token) {
+                .field => |val| {
+                    // Copy the incoming field slice to the row buffer.
+                    const ln: usize = f + val.len;
+
+                    if (isFirst) {
+                        // Copy all the fields in the first row to a separate buffer
+                        // to be retained throughout the lifetime of the program.
+                        std.mem.copy(u8, self.hdrBuf[f..ln], val);
+                        try fields.append(self.hdrBuf[f..ln]);
+                    } else {
+                        // Row buffer can be discarded after processing each individual row.
+                        std.mem.copy(u8, self.rowBuf[f..ln], val);
+                        try fields.append(self.rowBuf[f..ln]);
+                    }
+
+                    f = ln;
+                },
+                .row_end => {
+                    f = 0;
+
+                    // Move the first row (header) fields to be reused with every subsequent
+                    // row as JSON keys.
+                    if (isFirst) {
+                        isFirst = false;
+                        self.keys = fields.toOwnedSlice();
+                        continue;
+                    }
+
+                    try self.writeJSON(fields, line);
+                    try fields.resize(0);
+
+                    line += 1;
+                },
+            }
+        }
+
+        try self.out.flush();
+
+        return line;
+    }
+
+    // Writes a list of "value" fields from a row as a JSON dict.
+    fn writeJSON(self: *Converter, vals: std.ArrayList([]const u8), line: u64) !void {
+        if (self.keys.len != vals.items.len) {
+            std.debug.print("Invalid field count on line {d}: {d} (headers) != {d} (fields).\n", .{ line, self.keys.len, vals.items.len });
+            return;
+        }
+
+        try self.out.writer().writeAll("{");
+
+        for (self.keys) |key, n| {
+            // Write the key.
+            try self.out.writer().writeAll("\"");
+            try self.out.writer().writeAll(key);
+            try self.out.writer().writeAll("\": ");
+
+            // Write the value.
+            if (vals.items[n].len == 0) {
+                try self.out.writer().writeAll("null");
+            } else {
+                try self.writeValue(vals.items[n], true);
+            }
+
+            // If it's not the last key, write a comma.
+            if (n < self.keys.len - 1) {
+                try self.out.writer().writeAll(", ");
+            }
+        }
+
+        try self.out.writer().writeAll("}\n");
+    }
+
+    fn writeValue(self: *Converter, val: []const u8, detectNum: bool) !void {
+        // If the first char is a digit, then try and iterate through the rest
+        // of the chars to see if it's a number.
+        if (detectNum and std.ascii.isDigit(val[0])) {
+            var hasPeriod: bool = false;
+            var isNum: bool = true;
+            for (val) |c| {
+                // Already found a period.
+                if (hasPeriod) {
+                    isNum = false;
+                    break;
+                }
+                if (c == '.') {
+                    hasPeriod = true;
+                }
+                if (!std.ascii.isDigit(c)) {
+                    isNum = false;
+                    break;
+                }
+            }
+
+            if (isNum) {
+                try self.out.writer().writeAll(val);
+                return;
+            }
+        }
+
+        // It's a string.
+        try self.out.writer().writeAll("\"");
+
+        // Iterate through the string to see if there are any quotes to escape.
+        // If there aren't, doing a writeAll() is faster than than doing a .write() per character.
+        if (self.hasQuote(val)) {
+            for (val) |c| {
+                // Escape quotes.
+                if (c == '"') {
+                    _ = try self.out.writer().write("\\");
+                }
+                _ = try self.out.writer().write(&[_]u8{c});
+            }
+        } else {
+            try self.out.writer().writeAll(val);
+        }
+        try self.out.writer().writeAll("\"");
+    }
+
+    fn hasQuote(self: *Converter, val: []const u8) bool {
+        for (val) |c| {
+            if (c == '"') {
+                return true;
+            }
+        }
+        return false;
+    }
+};
---
+++ src/main.zig
@@ -0,0 +1,80 @@
+const std = @import("std");
+const clap = @import("clap");
+const csv = @import("csv");
+const Converter = @import("./converter.zig").Converter;
+
+// Print flag usage to stdout.
+pub fn printHelp(comptime params: []const clap.Param(clap.Help)) !void {
+    std.debug.print("Convert CSV to JSON files.\n", .{});
+    try clap.help(
+        std.io.getStdErr().writer(),
+        params,
+    );
+}
+
+pub fn printStats(start: u64, end: u64, lines: u64) void {
+    const diff = @intToFloat(f64, end - start);
+    const ln = @intToFloat(f64, lines);
+
+    var val: f64 = 0;
+    var unit: []const u8 = undefined;
+
+    if (diff < std.time.ns_per_us) {
+        val = diff;
+        unit = "nano";
+    } else if (diff < std.time.ns_per_ms) {
+        val = diff / @intToFloat(f64, std.time.ns_per_us);
+        unit = "micro";
+    } else if (diff < std.time.ns_per_s) {
+        val = diff / @intToFloat(f64, std.time.ns_per_ms);
+        unit = "milli";
+    } else {
+        val = diff / @intToFloat(f64, std.time.ns_per_s);
+    }
+
+    var rate = ln / (diff / @intToFloat(f64, std.time.ns_per_s));
+    if (rate > ln) {
+        rate = ln;
+    }
+
+    std.debug.print("Processed {d} lines in {d:.2} {s}seconds ({d:.2} lines / second)\n", .{ lines, val, unit, rate });
+}
+
+pub fn main() !void {
+    const params = comptime [_]clap.Param(clap.Help){
+        clap.parseParam("-h, --help   Display this help and exit.") catch unreachable,
+        clap.parseParam("-i, --in <STR>   Path to input CSV file.") catch unreachable,
+        clap.parseParam("-a, --array <BOOL>   Convert rows to arrays instead of JSON lines.") catch unreachable,
+        clap.parseParam("-b, --buf <UINT>   Line buffer (default: 4096). Should be greater than the longest line.") catch unreachable,
+    };
+
+    // Help / usage.
+    var args = clap.parse(clap.Help, &params, .{}) catch |err| {
+        std.debug.print("invalid flags: {s}\n\n", .{@errorName(err)});
+        try printHelp(&params);
+        std.os.exit(1);
+    };
+    defer args.deinit();
+
+    if (args.flag("--help")) {
+        try printHelp(&params);
+        std.os.exit(1);
+    }
+
+    if (args.option("--in")) |fPath| {
+        std.debug.print("Reading {s} ...\n", .{fPath});
+
+        var timer = try std.time.Timer.start();
+        const start = timer.read();
+
+        var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        var conv = try Converter.init(&arena.allocator, std.io.getStdOut().writer(), 4096);
+        const lines = try conv.convert(fPath);
+
+        printStats(start, timer.read(), lines);
+        std.os.exit(0);
+    }
+
+    // No flags.
+    try printHelp(&params);
+}
